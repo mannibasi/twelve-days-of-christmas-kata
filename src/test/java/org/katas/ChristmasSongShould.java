27ee51028adf9257fed23146dbb112fb872d79b8
= <manni.basi@gmail.com>
2023-11-19 17:23:51 +0000

    Incrementally added first 3 tests with 'obvious' implementations

--- src/test/java/org/katas/ChristmasSongShould.java
+++ src/test/java/org/katas/ChristmasSongShould.java
@@ -2,6 +2,8 @@ package org.katas;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ChristmasSongShould {
 
     @Test
@@ -9,5 +11,22 @@ public class ChristmasSongShould {
         assertEquals(ChristmasSong.getVerse(1), "On the First day of Christmas" + System.lineSeparator() +
                 "My true love gave to me:" + System.lineSeparator() +
                 "A partridge in a pear tree");
+    }
+
+    @Test
+    public void ReturnSecondVerseOfTheSongWhenCalledWithTwo() {
+        assertEquals(ChristmasSong.getVerse(2), "On the Second day of Christmas" + System.lineSeparator() +
+                "My true love gave to me:" + System.lineSeparator() +
+                "Two turtle doves," + System.lineSeparator() +
+                "And a partridge in a pear tree");
+    }
+
+    @Test
+    public void ReturnThirdVerseOfTheSongWhenCalledWithThree() {
+        assertEquals(ChristmasSong.getVerse(3), "On the Third day of Christmas" + System.lineSeparator() +
+                "My true love gave to me:" + System.lineSeparator() +
+                "Three French hens," + System.lineSeparator() +
+                "Two turtle doves," + System.lineSeparator() +
+                "And a partridge in a pear tree");
     }
 }
