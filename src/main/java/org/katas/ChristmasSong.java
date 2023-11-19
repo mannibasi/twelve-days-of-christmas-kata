41fec9a4e2fd5c09d2f00eca417928c80cd67a5d
= <manni.basi@gmail.com>
2023-11-19 17:57:52 +0000

    Refactored with new helper method

--- src/main/java/org/katas/ChristmasSong.java
+++ src/main/java/org/katas/ChristmasSong.java
@@ -2,19 +2,39 @@ package org.katas;
 
 public class ChristmasSong {
     public static String getVerse(int i) {
+        StringBuilder verse = new StringBuilder();
+        verse.append("On the")
+                .append(" ")
+                .append(getNumberAsWord(i))
+                .append(" ")
+                .append("day of Christmas")
+                .append(System.lineSeparator())
+                .append("My true love gave to me:")
+                .append(System.lineSeparator());
+
+        switch (i) {
+            case 3:
+                verse.append("Three French hens,")
+                        .append(System.lineSeparator());
+            case 2:
+                verse.append("Two turtle doves,")
+                        .append(System.lineSeparator());
+            case 1:
+                if(i > 1) {
+                    verse.append("And a partridge in a pear tree");
+                }
+                else {
+                    verse.append("A partridge in a pear tree");
+                }
+        }
+        return verse.toString();
+    }
+
+    private static String getNumberAsWord(int i) {
         if (i == 1)
-            return "On the First day of Christmas" + System.lineSeparator() +
-                    "My true love gave to me:" + System.lineSeparator() +
-                    "A partridge in a pear tree";
+            return "First";
         if (i == 2)
-            return "On the Second day of Christmas" + System.lineSeparator() +
-                "My true love gave to me:" + System.lineSeparator() +
-                "Two turtle doves," + System.lineSeparator() +
-                "And a partridge in a pear tree";
-        return "On the Third day of Christmas" + System.lineSeparator() +
-                "My true love gave to me:" + System.lineSeparator() +
-                "Three French hens," + System.lineSeparator() +
-                "Two turtle doves," + System.lineSeparator() +
-                "And a partridge in a pear tree";
+            return "Second";
+        return "Third";
     }
 }
